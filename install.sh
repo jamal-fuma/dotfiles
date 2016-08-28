51c87a7ee5bcc35c3a178cab0e81300da5bb91f3
Jamal Natour <Jamal.Natour@fumasoftware.com>
2016-08-28 17:56:09 +0100

    Patch: added basic install script

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# workout the absolute path to the checkout directory
+abspath()
+{
+    case "${1}" in
+        [./]*)
+            local ABSPATH="$(cd ${1%/*}; pwd)/${1##*/}"
+            echo "${ABSPATH}"
+            ;;
+        *)
+            echo "${PWD}/${1}"
+            ;;
+    esac
+}
+
+SCRIPT=$(abspath ${0})
+ROOTPATH=`dirname ${SCRIPT}`
+export PROJECT_ROOT=${ROOTPATH}
+
+symlink_files()
+{
+	while read from to;
+	do
+		ln -sv ${from} ${to}
+	done <<EOS
+${PROJECT_ROOT}/vim/vimrc	${HOME}/.vimrc
+${PROJECT_ROOT}/git/gitconfig	${HOME}/.gitconfig
+EOS
+}
+
+case $1 in
+*)
+	symlink_files
+;;
+esac
